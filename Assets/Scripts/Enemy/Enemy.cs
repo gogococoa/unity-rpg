166054acc2cefdc004ba4c5b4d88b62a0725c696
gogococoa <saint2737@gmail.com>
2026-08-19 21:52:15 +0700

    feat: :sparkles: Timer & Delay enemy hit change color

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,9 +1,47 @@
+using System.Threading;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    private readonly float redColorDuration = 1;
+
+    private float currentTimeInGame;
+    private float lastTimeWasDamaged;
+
+    private Color originColor;
+
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        originColor = sr.color;
+    }
+
+    private void Update()
+    {
+        currentTimeInGame = Time.time;
+
+        ChangeColorToOriginColor();
+    }
+
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name + " took some damage");
+        sr.color = Color.red;
+
+        lastTimeWasDamaged = Time.time;
+    }
+
+    private void ChangeColorToOriginColor()
+    {
+        if (currentTimeInGame > lastTimeWasDamaged + redColorDuration && sr.color != originColor)
+        {
+            TurnOriginColor();
+        }
+    }
+
+    private void TurnOriginColor()
+    {
+        sr.color = originColor;
     }
 }
